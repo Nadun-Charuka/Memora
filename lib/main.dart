5134d59da31834e370f53f00fabccbe77514eb3c
Nadun Charuka <nadun.charuka2016@gmail.com>
2025-10-18 12:08:46 +0530

    start again

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:memora/features/splash/screens/splash_screen.dart';
 
 void main() {
   runApp(ProviderScope(child: const MyApp()));
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Memora',
-      home: SplashScreen(),
+      home: Scaffold(),
     );
   }
 }
